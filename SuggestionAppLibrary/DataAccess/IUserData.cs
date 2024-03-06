b46d6e936d56b00ccc5a9dd48197e79422a56010
Petrovic-Luka <lukapetrovic00@gmail.com>
2024-03-06 23:03:41 +0100

    Add project files.

---
+++ SuggestionAppLibrary/DataAccess/IUserData.cs
@@ -0,0 +1,12 @@
+﻿
+namespace SuggestionAppLibrary.DataAccess
+{
+    public interface IUserData
+    {
+        Task CreateUser(UserModel user);
+        Task<List<UserModel>> GetAllUsers();
+        Task<UserModel> GetUser(string id);
+        Task<UserModel> GetUserFromAuthentication(string objectId);
+        Task UpdateUser(UserModel user);
+    }
+}
